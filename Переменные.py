7cb163f529bcf0c2e7001299b881b1d38a37cf19
Besik86 <besik86@gmail.com>
2024-07-30 11:46:21 +0300

    Add files via upload

--- Переменные.py
+++ Переменные.py
@@ -1,4 +1,4 @@
-name = 'Pythone,'
-time = '1.5,'
-count = '12,'
-print('Курс:',name,'всего задач:', count,'затрачено часов',time,1.5/12)
+name = 'Pythone'
+time = 1.5
+count = 12
+print('Курс:',name,'всего задач:', count,'затрачено часов:',time,'среднее время выполнения',time/count)
